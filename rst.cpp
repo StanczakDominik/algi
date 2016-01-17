0402fd1fcabd8ec1f18642c7d1a77752e70afc72
Dominik Stańczak <stanczakdominik@gmail.com>
2016-01-17 19:55:52 +0100

    cos jest

--- rst.cpp
+++ rst.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <iostream>
 #include <iomanip>
+#include <math.h>
 using namespace std;
-#define maxb 31
+#define maxb 3
 
 struct node
 {
@@ -10,25 +11,61 @@ struct node
   node *next[2];
 };
 
-//adapted from http://stackoverflow.com/questions/13484943/print-a-binary-tree-in-a-pretty-way
-void postorder(node* p, int indent=0)
-{
-    if(p != NULL) {
-        if(p->next[0]) postorder(p->next[0], indent+4);
-        if(p->next[1]) postorder(p->next[1], indent+4);
-        if (indent) {
-            std::cout << std::setw(indent) << ' ';
-        }
-        cout<< p->key << "\n ";
-    }
-}
-
-
-
 int bit(int x, int i)
 {
   return (x>>i)&1;
 }
+
+void print_bits(int keyue)
+{
+    int mask = 1 << (maxb-1);
+
+    while (mask)
+    {
+        printf("%d", (mask & keyue) != 0);
+        mask >>= 1;
+    }
+}
+
+void postorder(node* p, int indent=0)
+{
+    if(p != NULL) {
+        if(p->next[1]) {
+            postorder(p->next[1], indent+4);
+        }
+        if (indent) {
+            std::cout << std::setw(indent) << ' ';
+        }
+        if (p->next[1]) std::cout<<" /\n" << std::setw(indent) << ' ';
+        // std::cout<< number_to_bit(p->key) << "\n ";
+        std::cout<< p->key << "\n ";
+        if(p->next[0]) {
+            std::cout << std::setw(indent) << ' ' <<" \\\n";
+            postorder(p->next[0], indent+4);
+        }
+    }
+}
+
+void postorderbits(node* p, int indent=0)
+{
+    if(p != NULL) {
+        if(p->next[1]) {
+            postorderbits(p->next[1], indent+4);
+        }
+        if (indent) {
+            std::cout << std::setw(indent) << ' ';
+        }
+        if (p->next[1]) std::cout<<" /\n" << std::setw(indent) << ' ';
+        print_bits(p->key);
+        cout << "\n ";
+        if(p->next[0]) {
+            std::cout << std::setw(indent) << ' ' <<" \\\n";
+            postorderbits(p->next[0], indent+4);
+        }
+    }
+}
+
+
 
 node *root;
 
@@ -47,7 +84,7 @@ node **Search(int v)
   {
     depth++;
     int this_bit = bit(v,b--);
-    cout << this_bit << endl;
+    printf("depth is %d, bit is %d\n", depth, this_bit);
     p = &((*p)->next[bit(v,b--)]);
   }
   printf("looking for %4d, depth: %d\n", v, depth);
@@ -64,17 +101,84 @@ void Insert(int v)
   (*p)->next[0] = (*p)->next[1] = NULL;
 }
 
+void JDelete(int v)
+{
+    if (!root)
+        return;
+
+    node **p = Search(v);
+
+    if (!(*p))
+        return;
+
+    node **pp = p;
+
+    while( (*pp)->next[0] || (*pp)->next[1] )
+    {
+        if( (*pp)->next[0] )
+            pp = &((*pp)->next[0]);
+        else
+            pp = &((*pp)->next[1]);
+    }
+
+    (*p)->key = (*pp)->key;
+    delete *pp;
+}
+
+void Delete(int v)
+{
+  node **p = Search(v);
+  if (!(*p)) return;
+
+  node *pp = *p;
+  while(true)
+  {
+    if(pp->next[0])
+    {
+      pp = pp->next[0];
+    }
+    else if(pp->next[1])
+    {
+      pp = pp->next[1];
+    }
+    else break;
+  }
+
+  pp->next[1] = (*p)->next[1];
+  pp->next[0] = (*p)->next[0];
+  node *temp = *p;
+  *p=pp;
+  delete temp;
+}
+
+void test_bits()
+{
+  for(int v=0;v<6;v++)
+  {
+    printf("%2d | %2d   %2d   %2d   %2d  |=  %2d\n", v, bit(v,0), bit(v,1), bit(v,2), bit(v,3), bit(v,0)+2*bit(v,1)+4*bit(v,2)+8*bit(v,3));
+  }
+}
 
 int main()
 {
-  int A[] = {1,2,4,8,3,5,6,7};//{4, 7, 9, 11, 8, 2};
-  for(int v=0;v<6;v++)
+  test_bits();
+  int A[] = {1,2,3,4,5,6,7};
+  for(int v=0;v<8;v++)
   {
     Insert(A[v]);
-    printf("%2d\n", (*Search(A[v]))->key);
-    // printf("%2d | %2d   %2d   %2d   %2d  |=  %2d\n", v, bit(v,0), bit(v,1), bit(v,2), bit(v,3), bit(v,0)+2*bit(v,1)+4*bit(v,2)+8*bit(v,3));
   }
 
   postorder(root);
-  Search(100);
+  cout << "===========================" << endl;
+  postorderbits(root);
+  Delete(5);
+  // postorder(root);
+  if (*(Search(7))==NULL)
+  {
+    cout << "nope" << endl;
+  }
+  else
+  {
+    cout << (*(Search(7)))->key << endl;
+  }
 }
